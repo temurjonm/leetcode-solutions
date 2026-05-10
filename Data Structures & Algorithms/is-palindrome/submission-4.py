b1be8f65bebc1ee36959422a0d5b186b0f5e9b10
Temurjon M <mtimur0385@gmail.com>
2026-05-10 07:55:56 -0500

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,27 @@
+'''
+    "Was it a car or a cat I saw?"
+    'was it a car or a cat i saw'
+     L
+                               R 
+'''
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        left, right = 0, len(s) - 1
+
+        while left < right:
+
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left] != s[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
